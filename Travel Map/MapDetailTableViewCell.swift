f1fc1e4b10a112b25b459e5919292e904b246649
Liam Sun <slkonichiwa@icloud.com>
2016-11-03 17:04:12 +0900

    modify for swift 2.2

---
+++ Travel Map/MapDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MapDetailTableViewCell.swift
+//  Travel Map
+//
+//  Created by Sun liang on 2/27/16.
+//  Copyright © 2016 Liam. All rights reserved.
+//
+
+import UIKit
+
+class MapDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
